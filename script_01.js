bbf2caec1256e8001487531d455061e7e0ca88eb
Peter <schmidt.multimedia@gmail.com>
2021-02-10 10:30:47 +0100

    02a_Parametrisierung / Daten von INNEN

--- script_01.js
+++ script_01.js
@@ -2,9 +2,24 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsrumpf (body)
 function test()
 {
     console.log("Hallo Drazen!");
 }
 
-test();
+// Funktionsaufruf (call)
+//test();
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen() 
+{
+    let firstName = "Ahmad"; // Variable 
+    console.log("Hallo " + firstName + "!");
+}
+
+//ausgabeNamen();
+//console.log(firstName); // Fehler --> lokal
+
